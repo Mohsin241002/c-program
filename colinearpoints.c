5c2a41623991adfd58f15572826b561c07441b9a
mohsin <chunawala246@gmail.com>
2021-09-01 10:45:25 +0530

    colineat points

--- colinearpoints.c
+++ colinearpoints.c
@@ -1,9 +1,20 @@
 #include<stdio.h>
 
-int main(){
-    int x1, x2,x3,y1,y2,y3;
-    printf("Input cordinates of 1st point\n");
+void main(){
+    int x1, x2,x3,y1,y2,y3,m1,m2;
+    printf("The program to find points are colinear or not\n");
+    printf("NOTE: while entering the values the 1st no is cordinates of x and 2nd is of y");
+    printf("\nInput cordinates of 1st point\n");
     scanf("%d%d",&x1 ,&y1);
-    printf("\n%d %d", x1 , y1);
-    return 0;
+    printf("\nInput cordinates of 2nd point\n");
+    scanf("%d%d",&x2 ,&y2);
+    printf("\nInput cordinates of 3nd point\n");
+    scanf("%d%d",&x3 ,&y3);
+    m1=((x2-x1)/(y2-y1));
+    m2=((x3-x2)/(y3-y2));
+    if (m1==m2)
+        printf("The points are Colinear and cannot form a triangle");
+    else
+        printf("The points are not colinear");
+    
 }
