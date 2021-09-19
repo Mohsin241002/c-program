a993cf99d05978892f3241bbb5506d9326860860
mohsin <chunawala246@gmail.com>
2021-09-19 12:57:46 +0530

    format changes

--- forloop1-10.c
+++ forloop1-10.c
@@ -1,12 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    int a, sum , n ;
+int main()
+{
+    int a, sum, n;
     printf("Program to add sum of first n natural nos is =: \n");
     printf("Input the amount of nos u want to add =:");
     scanf("%d", &n);
-    for(sum=0, a=1; a<=n ; a++){
-        sum=sum+a;
+    for (sum = 0, a = 1; a <= n; a++)
+    {
+        sum = sum + a;
     }
     printf("\nthe sum of all nos is = %d", sum);
     return 0;
