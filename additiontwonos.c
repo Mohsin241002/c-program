89cbc4e21f7fff2a602ed1962225c76b143dbf97
mohsin <chunawala246@gmail.com>
2021-12-01 09:26:52 +0530

    online c lecture

---
+++ additiontwonos.c
@@ -0,0 +1,12 @@
+// find addition of two nos
+
+#include<stdio.h>
+
+void main(){
+    int a,b;
+    printf("Input first no = ");
+    scanf("%d",&a);
+    printf("Input second no = ");
+    scanf("%d",&b);
+    printf("the addition of %d and %d is %d",a,b,a+b);
+}
